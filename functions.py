3db3a21b0bf438b15034a7b6afe1d34576daf651
Gribek <piotrmaslewski@gmail.com>
2021-03-28 20:25:38 +0200

    Create functions.py file. Add ApiDataDownloader class.
    ApiDataDownloader class handles downloading data from API.
    Two methods added for this class:
    get_data - creates session, prepares all requests and collects data
    send_request - sends single request to the API.

---
+++ functions.py
@@ -0,0 +1,40 @@
+import requests
+
+
+class ApiDataDownloader:
+    """Download data from API."""
+
+    def __init__(self, url, parameters, timeout=5):
+        self.__api_url = url
+        self.__parameters = parameters
+        self.__timeout = timeout
+        self.__data = None
+
+    def get_data(self):
+        """Send all requests to the API and gather data."""
+        api_data = []
+        with requests.Session() as s:
+            for parameter_dict in self.__parameters:
+                data = self.send_request(s, parameter_dict)
+                if data is None:
+                    break
+                api_data += data
+            else:
+                self.__data = api_data
+
+    def send_request(self, session, params):
+        """Send a request using the given session and parameters."""
+        try:
+            response = session.get(self.__api_url, params=params,
+                                   timeout=self.__timeout)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print('HTTP Error:', err)
+        except requests.exceptions.Timeout as err:
+            print('Timeout Error:', err)
+        except requests.exceptions.ConnectionError as err:
+            print('Connection Error:', err)
+        except requests.exceptions.RequestException as err:
+            print('Request Error:', err)
+        else:
+            return response.json()
