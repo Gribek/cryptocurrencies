ece43e7e104600d9d2605f70dd376a3dce56a013
Gribek <piotrmaslewski@gmail.com>
2021-04-01 00:48:07 +0200

    Add new options in settings.
    Add api url for historical data.
    Add list of modifications to perform before saving historical data to db.
    Add limit for the number of dates asked in one request to api (historical data).

--- settings.py
+++ settings.py
@@ -1,6 +1,30 @@
+from string import Template
+
 # DATABASE
 # Database filename
 DB_FILENAME = 'crypto.db'
 
 # Relative path to the database file directory
 DB_PATH = 'db'
+
+
+# API
+# url for historical OHLC
+HISTORICAL_DATA = Template(
+    'https://api.coinpaprika.com/v1/coins/$coin/ohlcv/historical')
+
+# Maximum number of rows in one request (due to API limit)
+ROWS_LIMIT = 360
+
+
+# API DATA MODIFICATIONS
+# Configuration of the modifications to be performed on API data before
+# they can be saved in the database
+
+# Modifications for HistoricalValue model
+HISTORICAL_MODIFICATIONS = (
+    {
+        'function': 'format_date',
+        'args': ('time_close', 'date', '%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%d')
+    },
+)
