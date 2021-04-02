4d17a4152e4bc2934fd42662d2283264b2b79505
Gribek <piotrmaslewski@gmail.com>
2021-04-02 21:24:17 +0200

    Add possibility to use short date format YYYY-MM.
    Adjust validation functions accordingly. Add new function that changes the day
    of the datetime object to the last day of its month. Use this last function for
    values of end_date argument in case of input in short format (YYYY-MM).

--- validation.py
+++ validation.py
@@ -1,3 +1,5 @@
+from calendar import monthrange
+
 import click
 
 from functions import to_datetime
@@ -5,12 +7,26 @@ from functions import to_datetime
 
 def validate_start_date(ctx, value):
     """Validate start_date argument."""
-    try:
-        start_date = to_datetime(value)
-        ctx.obj['start'] = start_date
-    except ValueError:
-        raise click.BadParameter('Incorrect date format. Should be YYYY-MM-DD')
 
+    # Check that the date format is correct
+    format_match = False
+    for format_ in ('%Y-%m-%d', '%Y-%m'):
+        try:
+            start_date = to_datetime(value, format_)
+        except ValueError:
+            pass
+        else:
+            format_match = True
+
+    # Raise error if no format matches
+    if not format_match:
+        raise click.BadParameter(
+            'Incorrect date format. Should be YYYY-MM-DD or YYYY-MM')
+
+    # Add value to the context dictionary
+    ctx.obj['start'] = start_date
+
+    # Check that the start date is before the end date
     try:
         if start_date > ctx.obj['end']:
             raise click.BadParameter('Start date has to be before end date!')
@@ -22,12 +38,38 @@ def validate_start_date(ctx, value):
 
 def validate_end_date(ctx, value):
     """Validate end_date argument."""
-    try:
-        end_date = to_datetime(value)
-        ctx.obj['end'] = end_date
-    except ValueError:
-        raise click.BadParameter('Incorrect date format. Should be YYYY-MM-DD')
 
+    # Check that the date format is correct
+    format_match = False
+    modify_date = False
+    try:
+        end_date = to_datetime(value, '%Y-%m-%d')
+    except ValueError:
+        pass
+    else:
+        format_match = True
+
+    try:
+        end_date = to_datetime(value, '%Y-%m')
+    except ValueError:
+        pass
+    else:
+        format_match = True
+        modify_date = True
+
+    # Raise error if no format matches
+    if not format_match:
+        raise click.BadParameter(
+            'Incorrect date format. Should be YYYY-MM-DD or YYYY-MM')
+
+    # Set day to the last day of the month for format YYYY-MM
+    if modify_date:
+        end_date = last_day(end_date)
+
+    # Add value to the context dictionary
+    ctx.obj['end'] = end_date
+
+    # Check that the start date is before the end date
     try:
         if end_date < ctx.obj['start']:
             raise click.BadParameter('End date has to be after start date!')
@@ -35,3 +77,9 @@ def validate_end_date(ctx, value):
         pass
 
     return end_date
+
+
+def last_day(date):
+    """Set the day to the last day of the month."""
+    day = monthrange(date.year, date.month)[1]
+    return date.replace(day=day)
