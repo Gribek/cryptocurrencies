a9ea2d9f1f04c2e6c2f10b17d15acca442ea0c52
Gribek <piotrmaslewski@gmail.com>
2021-04-17 18:26:02 +0200

    Create a new class that handles database connection.
    Make sqlite_connection method of the new class and use modified settings
    to connect to the selected database.

--- db_connection.py
+++ db_connection.py
@@ -2,8 +2,24 @@ import os
 
 from peewee import SqliteDatabase
 
+from settings import DATABASE
 
-def sqlite_connection(path, filename):
-    """Prepare a connection to the selected SQLite database."""
-    return SqliteDatabase(database=os.path.join(path, filename),
-                          pragmas={'foreign_keys': 1})
+
+class DatabaseConnection:
+    """Connect to the database.
+
+    The connection method is selected based on the DATABASE
+    setting in settings.py file.
+    """
+
+    @classmethod
+    def connect(cls):
+        """Connect and return the database object."""
+        connection = getattr(cls, DATABASE['connection'])
+        return connection(**DATABASE['parameters'])
+
+    @staticmethod
+    def sqlite_connection(path, filename):
+        """Prepare a connection to the selected SQLite database."""
+        return SqliteDatabase(database=os.path.join(path, filename),
+                              pragmas={'foreign_keys': 1})
